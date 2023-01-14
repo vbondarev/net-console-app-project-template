8cc3ba95f7de5bbc72d1db02e4afa7fef9ff7122
v.bondarev <bondarev.vladimir.87@gmail.com>
2023-01-15 02:30:13 +0600

    Uploaded project template

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace HostedNetConsoleAppProjectTemplate;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var host =  CreateHostBuilder(args).Build();
+
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host
+            .CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((_, builder) =>
+            {
+                var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                builder
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{environment}.json", true);
+            })
+            .ConfigureLogging((_, builder) =>
+            {
+                builder.AddSerilog();
+            });
+    }
+}
